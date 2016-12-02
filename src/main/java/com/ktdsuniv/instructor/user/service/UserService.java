d4064ee9e816326c8221f2bdebf400bc469c7da5
subin119 <sungman171@naver.com>
2016-12-02 09:15:32 +0900

    no message

--- src/main/java/com/ktdsuniv/instructor/user/service/UserService.java
+++ src/main/java/com/ktdsuniv/instructor/user/service/UserService.java
@@ -1,10 +1,13 @@
 package com.ktdsuniv.instructor.user.service;
 
-import java.util.List;
+import javax.servlet.http.HttpSession;
 
+import user.schema.InstructorsSchema;
 import user.schema.UsersSchema;
 
 public interface UserService {
+
+	public boolean instructorSignIn(UsersSchema user, HttpSession session);
 
 
 }
--- src/main/java/com/ktdsuniv/instructor/user/web/UserController.java
+++ src/main/java/com/ktdsuniv/instructor/user/web/UserController.java
@@ -1,13 +1,17 @@
 package com.ktdsuniv.instructor.user.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.ktdsuniv.instructor.user.service.UserService;
 
+import user.schema.UsersSchema;
+
 @Controller
 public class UserController {
-
 	
 	private UserService userService;
 
@@ -19,4 +23,24 @@ public class UserController {
 	public String test(){
 		return "/test";
 	}
+	
+	@RequestMapping("/instructorSignIn")
+	public ModelAndView viewSignInPage() {
+		ModelAndView view = new ModelAndView();
+		view.setViewName("instructorSignIn");
+		return view;
+	}
+	
+	@RequestMapping("/instructorDoSignIn")
+	public String doSignInAction(UsersSchema user, HttpSession session) {
+		userService.instructorSignIn(user, session);
+		return "redirect:/main";
+	}
+	
+	@RequestMapping("/instructorSignOut")
+	public String doSignOutAction(HttpSession session) {
+		session.invalidate();
+		return "redirect:/instructorSignIn";
+	}
+
 }
